eaa5e5d1672a2dc11399ff983d422c0a4c2cf311
Andrea <andrea.berrocal95@gmail.com>
2023-03-30 13:09:34 -0700

    fix to name of test

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders app and checks if nasa logo present', () => {
   render(<App />);
   //const linkElement = screen.getByText(/Copyright/i);
   const linkElement = screen.getByAltText(/nasa logo/i)
