1a7b5ce05ca9f2f99db949132ecc80342887e8f1
Andrea <andrea.berrocal95@gmail.com>
2023-03-30 13:02:02 -0700

    change background to CSS

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import "./App.scss";
 import { Astronomy } from "./components/Astronomy";
-import ParticleBackground from "react-particle-backgrounds";
-import { particles } from "./particles";
 
 function App() {
   return (
     <div className="App">
-      <ParticleBackground settings={particles} />
-      <h1 className="title">Astronomy Picture Of The Day</h1>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
       <Astronomy />
     </div>
   );
