cb3346f04146b3cfc0e68ca3c14655935bbf27e3
Andrea <andrea.berrocal95@gmail.com>
2023-03-30 13:08:59 -0700

    add test for nasa logo

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Astronomy Picture/i);
+  //const linkElement = screen.getByText(/Copyright/i);
+  const linkElement = screen.getByAltText(/nasa logo/i)
   expect(linkElement).toBeInTheDocument();
 });
