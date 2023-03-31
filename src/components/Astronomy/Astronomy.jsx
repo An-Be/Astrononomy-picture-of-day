5b2cc85022a4593d1616595ea50ac8878fb03743
Andrea <andrea.berrocal95@gmail.com>
2023-03-31 07:58:49 -0700

    if no copyright do not show copyright section

--- src/components/Astronomy/Astronomy.jsx
+++ src/components/Astronomy/Astronomy.jsx
@@ -5,15 +5,16 @@ import Loader from "../Loader/Loader";
 
 export const Astronomy = () => {
   const [pictureOfDay, setPictureOfDay] = useState(null);
-  const [error, setError] = useState(null)
+  const [error, setError] = useState(null);
 
   const fetchPictureOfDay = async () => {
-    try{
+    try {
       const res = await fetch(API_URL);
       const data = await res.json();
       setPictureOfDay(data);
-    }catch(e){
-      setError('Sorry something went wrong, please try again.')
+      console.log(data);
+    } catch (e) {
+      setError("Sorry something went wrong, please try again.");
     }
   };
 
@@ -23,31 +24,40 @@ export const Astronomy = () => {
 
   return (
     <>
-    {error && <h1 className="error">{error}</h1>}
-    {!pictureOfDay && !error ? <div className="loader-wrapper"><Loader /></div> :
-    <div className="wrapper">
-      <div className="wrapper__top-wrapper">
-        <img
-          className="wrapper__top-wrapper__nasa"
-          src={require("../../images/nasa_logo.png")}
-          alt="nasa logo"
-        />
-        <h1 className="wrapper__top-wrapper__title">{pictureOfDay?.title}</h1>
-        <img
-          className="wrapper__top-wrapper__image"
-          src={pictureOfDay?.url}
-          alt="pic of day"
-        />
-        <p className="wrapper__bottom-wrapper__copyright">
-          Copyright: {pictureOfDay?.copyright}
-        </p>
-      </div>
-      <div className="wrapper__bottom-wrapper">
-        <p className="wrapper__bottom-wrapper__explanation">
-          {pictureOfDay?.explanation}
-        </p>
-      </div>
-    </div> }
+      {error && <h1 className="error">{error}</h1>}
+      {!pictureOfDay && !error ? (
+        <div className="loader-wrapper">
+          <Loader />
+        </div>
+      ) : (
+        <div className="wrapper">
+          <div className="wrapper__top-wrapper">
+            <img
+              className="wrapper__top-wrapper__nasa"
+              src={require("../../images/nasa_logo.png")}
+              alt="nasa logo"
+            />
+            <h1 className="wrapper__top-wrapper__title">
+              {pictureOfDay?.title}
+            </h1>
+            <img
+              className="wrapper__top-wrapper__image"
+              src={pictureOfDay?.url}
+              alt="pic of day"
+            />
+            {pictureOfDay?.copyright ? (
+              <p className="wrapper__bottom-wrapper__copyright">
+                Copyright: {pictureOfDay?.copyright}
+              </p>
+            ) : null}
+          </div>
+          <div className="wrapper__bottom-wrapper">
+            <p className="wrapper__bottom-wrapper__explanation">
+              {pictureOfDay?.explanation}
+            </p>
+          </div>
+        </div>
+      )}
     </>
   );
 };
